3b3c3e6cc96ef0c1f7b317c9a3eb8890113f0f26
Delisa Mason <iskanamagus@gmail.com>
2013-07-08 23:19:01 -0400

    Cleanup

--- cocoadocs/Cocoadocs.m
+++ cocoadocs/Cocoadocs.m
@@ -1,10 +1,25 @@
 //
-//  cocoadocs.m
-//  cocoadocs
+//  Cocoadocs.m
 //
-//  Created by Delisa Mason on 7/8/13.
-//  Copyright (c) 2013 Delisa Mason. All rights reserved.
+//  Copyright (c) 2013 Delisa Mason. http://delisa.me
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//  IN THE SOFTWARE.
 
 #import "Cocoadocs.h"
 
@@ -41,8 +56,6 @@ static NSString *XAR_EXECUTABLE = @"/usr/bin/xar";
     return self;
 }
 
-#pragma mark - Private
-
 - (void)installOrUpdateDocSetsForPods {
     for (NSString *podName in [self installedPodNamesInWorkspace]) {
         NSURL *docsetURL = [NSURL URLWithString:[NSString stringWithFormat:DOCSET_ARCHIVE_FORMAT, podName]];
@@ -55,6 +68,8 @@ static NSString *XAR_EXECUTABLE = @"/usr/bin/xar";
         }];
     }
 }
+
+#pragma mark - Private
 
 - (void)extractPath:(NSString *)path {
     NSTask *task = [NSTask new];
